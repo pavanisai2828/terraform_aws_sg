cb968a3a36c3586646bcbeb2e67c007da9faf092
pavanisai2828 <pavani.sai1313@gmailcom>
2026-07-25 14:36:00 +0530

    hello world script created

--- locals.tf
+++ locals.tf
@@ -1,14 +1,11 @@
 locals {
   common_tags={
     type=map(any)
-    default={
         "Project"= var.Project
         "Environment"=var.Environment
          Terraform= true
          Name= local.common_name
-
-    }
   }
-  common_name="${local.common_tags.Project}-${local.common_tags.Environment}-${var.sg_name}" #roboshop-dev-mongodb
+  common_name="${var.Project}-${var.Environment}-${var.sg_name}" #roboshop-dev-mongodb
   
 }
